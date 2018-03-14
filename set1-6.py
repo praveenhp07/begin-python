98d8a9831fe9d3af2413aa68b298c7f6317eed87
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-14 02:16:46 -0700

    Update set1-6.py

--- set1-6.py
+++ set1-6.py
@@ -3,9 +3,9 @@ def main():
     try:
 
         if(int(year)%4==0 and int(year)%100!=0 or int(year)%400==0):
-            print("The year is a leap year!")
+            print("This year is a leap year!")
         else:
-            print("The year is not a leap year!")
+            print("This year is not a leap year!")
     except:
         print("Invalid entry")
 if __name__ == '__main__':
