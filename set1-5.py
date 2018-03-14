e5f0f5190518611960a3a49c691f6239ffd3c5cd
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-13 23:22:27 -0700

    Create set1-5.py

---
+++ set1-5.py
@@ -0,0 +1,10 @@
+def main():
+    a=input("Enter a Alphabet=")
+    if(a>='a'):
+      print(a+" is an Alphabet")
+    elif(a>='A'):
+      print(a+" is an Alphabet")
+    else:
+      print(a+" is not an Alphabet")
+if __name__ == '__main__':
+    main()
