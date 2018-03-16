7a109778e4c4af929ad7060dc6b7c54644d9d532
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-16 09:30:53 -0700

    Update sum of k int from n array.py

--- sum of k int from n array.py
+++ sum of k int from n array.py
@@ -2,10 +2,10 @@ try:
     n=int(input("enter no. of terms"))
     i=0
     a=[int(input()) for i in range(0,n)]
-    b=int(input("Enter no of terms to be added:"))
+    k=int(input("Enter no of terms to be added:"))
     s=0
     i=0
-    while(i<b):
+    while(i<k):
         s=int(s)+a[i]
         i=i+1
     print("sum of integers is:"+str(s))
