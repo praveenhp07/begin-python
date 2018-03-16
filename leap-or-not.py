af07c57179a66353e33ed6a6c856adfd99c28498
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-16 09:20:48 -0700

    Update leap-or-not.py

--- leap-or-not.py
+++ leap-or-not.py
@@ -1,8 +1,8 @@
 def main():
-    year=input("Enter year to be checked:")
+    y=input("Enter year to be checked:")
     try:
 
-        if(int(year)%4==0 and int(year)%100!=0 or int(year)%400==0):
+        if(int(y)%4==0 and int(y)%100!=0 or int(y)%400==0):
             print("This year is a leap year!")
         else:
             print("This year is not a leap year!")
