0af13818ea70b7a66fee5f88b7b1f05960532bc2
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-16 15:19:51 +0530

    Create prime-num-bw-lim.py

---
+++ prime-num-bw-lim.py
@@ -0,0 +1,8 @@
+try:
+    x=int(input("Enter the lower limit for the range:"))
+    y=int(input("Enter the upper limit for the range:"))
+    for i in range(x+1,y):
+        if(i%2!=0):
+            print (+i)
+except:
+    print("Invalid entry")
