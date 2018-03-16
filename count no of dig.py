51d5bdea5e06ab31ff772aff64b830b6e61a54c5
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-16 09:33:01 -0700

    Update count no of dig.py

--- count no of dig.py
+++ count no of dig.py
@@ -1,5 +1,5 @@
 try:
-    n=str(input("enter to count no of integers:"))
-    print("the number of digits in integer is:")+str(len(n))
+    x=str(input("enter to count no of integers:"))
+    print("the number of digits in integer is:")+str(len(x))
 except:
     print("Invalid Entry")
