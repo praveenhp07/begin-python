ce2d752dea41ba8be502984ab6b973d47d00a82a
praveenhp07 <37235355+praveenhp07@users.noreply.github.com>
2018-03-17 11:24:40 +0530

    Update ams-or-not.py

--- ams-or-not.py
+++ ams-or-not.py
@@ -2,8 +2,8 @@ try:
     x=int(input("enter a num"))
     if(x>10000):
         print("enter less than 10000")
-        sum=0
-        a=x
+    sum=0
+    a=x
     while (a>0):
         dig=a%10
         sum+=dig**3
