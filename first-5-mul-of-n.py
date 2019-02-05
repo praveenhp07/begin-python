10e4aed76a8b8f9071d918557e36ac1fa85f2f41
Praveen <37235355+praveenhp07@users.noreply.github.com>
2019-02-05 15:49:39 +0530

    Update first-5-mul-of-n.py

--- first-5-mul-of-n.py
+++ first-5-mul-of-n.py
@@ -1,8 +1,8 @@
 try:
-    n=int(input("Enter a number:"))
-    x=1
-    for i in range (1,6):
-        x=n*i
-        print(x)
+	n=int(input())
+	x=1
+	for i in range (1,6):
+		x=n*i
+		print(x),
 except:
-    print("Invalid Entry")
+	print("Invalid Entry")
